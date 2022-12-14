c40ac440df7ac1190daf3cbba991b2eec0647399
Carl Fischer <Carl.Fischer.IV@gmail.com>
2022-12-13 23:08:02 -0500

    Switched from html to json logging.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/localizers/StateServer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/localizers/StateServer.java
@@ -1,11 +1,15 @@
 package org.firstinspires.ftc.teamcode.util.localizers;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 import fi.iki.elonen.NanoHTTPD;
 
 public class StateServer extends NanoHTTPD {
     public String state;
+    public JSONArray response;
     public int port = 8083;
     /**
      * Constructs an HTTP server on given port.
@@ -15,6 +19,18 @@ public class StateServer extends NanoHTTPD {
     public StateServer() throws IOException {
         super(8079);
         start(NanoHTTPD.SOCKET_READ_TIMEOUT,false);
+        response = new JSONArray();
+    }
+
+    public String getResponse(){
+        String retval = response.toString();
+        response = new JSONArray();
+        return retval;
+    }
+
+
+    public void addState(JSONObject obj){
+        response.put(obj);
     }
 
     /**
@@ -27,6 +43,6 @@ public class StateServer extends NanoHTTPD {
      */
     @Override
     public Response serve(IHTTPSession session) {
-        return newFixedLengthResponse(state);
+        return newFixedLengthResponse(Response.Status.OK, "application/json", getResponse());
     }
 }
