1713760a65fcc595516214d489310dfc28c0aa38
Innovators 3311 Robotics FTC <innovators3311@gmail.com>
2022-12-31 13:47:41 -0500

    Fixed type-o in MecanumDriveBase. Created a codebase for testing vuforia locations.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpVuforiaTesting.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.util.MecanumDriveBase;
+import org.firstinspires.ftc.teamcode.util.localizers.CombinedLocalizer;
+
+@TeleOp(name="Vuforia Testing", group="Testing")
+public class TeleOpVuforiaTesting extends OpMode
+
+{
+    private CombinedLocalizer localizer        = null;
+    private MecanumDriveBase mecanumDriveBase = null;
+//    private TowerController towerController;
+    private PacManTurnToPos pacMan;
+
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+        localizer = new CombinedLocalizer(hardwareMap);
+        mecanumDriveBase = new MecanumDriveBase(hardwareMap);
+//        towerController = new TowerController(hardwareMap);
+        pacMan = new PacManTurnToPos(localizer, mecanumDriveBase);
+        double max;
+    }
+    @Override
+    public void loop() {
+        localizer.displayTelemetry(telemetry);
+        localizer.handleTracking();
+        mecanumDriveBase.gamepadController(gamepad1);
+        mecanumDriveBase.driveBaseTelemetry(telemetry);
+        telemetry.addData("TeleOp heading", localizer.getHeading());
+        pacMan.handlePacMan(gamepad1, telemetry);
+        telemetry.update();
+    }
+}
