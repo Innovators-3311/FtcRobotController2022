192c191f869a02c071281b345a24613d7066db17
Samuel <sjmesloh@gmail.com>
2023-01-09 19:18:34 -0500

    Push this

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.util.DriveForTest;
+import org.firstinspires.ftc.teamcode.util.MecanumDriveBase;
+import org.firstinspires.ftc.teamcode.util.TowerController;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="BackUp", group="Basic drive")
+public class TeleOp extends OpMode
+
+{
+    private MecanumDriveBase mecanumDriveBase = null;
+    private TowerController towerController;
+    private DriveForTest driveForTest;
+
+    public void init()
+    {
+        telemetry.addData("Status", "Initialized");
+        towerController = new TowerController(hardwareMap, telemetry);
+        driveForTest = new DriveForTest(hardwareMap);
+    }
+    @Override
+    public void loop()
+    {
+        towerController.handleGamepad(gamepad2, telemetry);
+        driveForTest.drive(gamepad1);
+        telemetry.update();
+    }
+}
