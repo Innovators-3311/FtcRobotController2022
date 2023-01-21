ea13624087acbe0db8720669af02a8e30a6a1dbb
Innovators 3311 Robotics FTC <innovators3311@gmail.com>
2023-01-21 09:05:02 -0500

    we Tested localizer got little bit side tracked

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/odometry/JunctionAligner.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.util.odometry;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+public class JunctionAligner {
+    DistanceSensor leftSensor;
+    DistanceSensor rightSensor;
+
+    public double xCorrection;
+
+    public void AlignRobot(){
+        if(gamepad1.a){
+            if (leftSensor.getDistance(DistanceUnit.CM) <= 25 && rightSensor.getDistance(DistanceUnit.CM) <= 25){
+                //1 + 1 = 2 -Auggie
+                xCorrection = -leftSensor.getDistance(DistanceUnit.CM) + rightSensor.getDistance(DistanceUnit.CM) / 2 * (14/20);
+            }
+        }
+    }
+}
