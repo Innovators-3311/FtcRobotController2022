a9fea008268240b8d06f6b0099901618603c0416
Samuel <sjmesloh@gmail.com>
2023-01-24 20:46:57 -0500

    FreightFenzyBot Coded

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/FreightFreanzyBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/FreightFreanzyBot.java
@@ -14,6 +14,7 @@ public class FreightFreanzyBot extends OpMode
 {
     private MecanumDriveBase mecanumDriveBase = null;
     private DcMotor arm;
+    private DcMotor wheel;
 
     public void init()
     {
@@ -21,6 +22,9 @@ public class FreightFreanzyBot extends OpMode
         arm = hardwareMap.get(DcMotor.class, "arm");
         arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         arm.setDirection(DcMotorSimple.Direction.REVERSE);
+        wheel = hardwareMap.get(DcMotor.class, "wheel");
+        wheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        wheel.setDirection(DcMotorSimple.Direction.REVERSE);
         mecanumDriveBase = new MecanumDriveBase(hardwareMap, false);
     }
     @Override
@@ -28,6 +32,7 @@ public class FreightFreanzyBot extends OpMode
     {
         mecanumDriveBase.gamepadController(gamepad1);
         driveArm(gamepad1);
+        spinnyWheel(gamepad1);
     }
 
     private void driveArm(Gamepad gamepad)
@@ -43,6 +48,22 @@ public class FreightFreanzyBot extends OpMode
         else
         {
             arm.setPower(0);
+        }
+    }
+
+    private void spinnyWheel(Gamepad gamepad)
+    {
+        if (gamepad.right_bumper)
+        {
+            wheel.setPower(1);
+        }
+        else if (gamepad.left_bumper)
+        {
+            wheel.setPower(-1);
+        }
+        else
+        {
+            wheel.setPower(0);
         }
     }
 }
